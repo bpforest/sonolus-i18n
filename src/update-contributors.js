eed0f0b6e00eaa3ca95b72e44349e03f72a970ed
Nanashi. <sevenc7c@sevenc7c.com>
2025-05-30 15:49:44 +0900

    feat(update-contributors): Remove duplicated entries (#301)

--- src/update-contributors.js
+++ src/update-contributors.js
@@ -1,7 +1,25 @@
-import 'zx/globals'
-import fs from 'fs-extra'
+import "zx/globals";
+import fs from "fs-extra";
 
-const contributors = [...new Set((await $`git log --pretty="%aN"`).stdout.split('\n'))]
-    .filter((line) => !!line)
-    .sort()
-fs.outputJsonSync('./src/contributors.json', contributors, { spaces: 4 })
+const excludes = [
+  // Bots
+  "mt-gitlocalize",
+  "gitlocalize-app[bot]",
+
+  // Duplicates
+  // - Burrito
+  "NonSpicyBurrito",
+  // - ドマオー
+  "Dosugamea",
+  // - Nanashi.
+  "sevenc-nanashi",
+  "Nanashi",
+];
+
+const contributors = [
+  ...new Set((await $`git log --pretty="%aN"`).stdout.split("\n")),
+]
+  .filter((line) => !!line)
+  .filter((line) => !excludes.includes(line))
+  .sort();
+fs.outputJsonSync("./src/contributors.json", contributors, { spaces: 4 });
